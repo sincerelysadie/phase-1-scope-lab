7643e8df9193fac25f48549856fa394aaff5b80d
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-04-29 18:55:51 -0700

    commit tree 2efffb508fdd54ee0782dc8b667f7178b1785669

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+// if (customerName === "bob") {
+//     var customerName = upperCaseCustomerName;
+//     console.log(customerName); 
+//     upperCaseCustomerName + customerName;
+// }
+function upperCaseCustomerName() {
+    customerName= customerName.toUpperCase();
+    return customerName;
+}
+var bestCustomer 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
